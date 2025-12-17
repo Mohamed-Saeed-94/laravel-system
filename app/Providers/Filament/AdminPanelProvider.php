fd53bfd2a6f90f9d4c728f4395dfa8b424dcdbce
mohamed-saeed-94 <mohamed.said5010@gmail.com>
2025-12-18 02:16:09 +0300

    وصف التعديل

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -38,6 +38,10 @@ class AdminPanelProvider extends PanelProvider
                 Dashboard::class,
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->discoverResources(
+                in: base_path('Modules/Core/Filament/Resources'),
+                for: 'Modules\\Core\\Filament\\Resources'
+            )
             ->widgets([
                 AccountWidget::class,
                 FilamentInfoWidget::class,
