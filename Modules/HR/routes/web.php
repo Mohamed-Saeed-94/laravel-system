3dd9bdd5c5756792db9c30a51eef9b7460612849
mohamed-saeed-94 <mohamed.said5010@gmail.com>
2025-12-18 01:28:05 +0300

    وصف التعديل

--- Modules/HR/routes/web.php
+++ Modules/HR/routes/web.php
@@ -3,6 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use Modules\HR\Http\Controllers\HRController;
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('hrs', HRController::class)->names('hr');
-});
+Route::resource('hrs', HRController::class)->names('hr');
